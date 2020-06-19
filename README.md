1db73908b794c90d0e2528dda58191aec090b2e6
Olohundare Nasirudeen <naskkographics@gmail.com>
2020-06-19 15:22:20 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Express Redis Rate Limit
+
+Basic rate-limiting middleware for Express. Use to limit repeated requests to public APIs and/or endpoints such as password reset.
+
+# Installation
+
+```
+$ npm install express-redis-rate-limit
+```
+
+# Usage
+
+```
+const RateLimit = require("express-redis-rate-limit");
+const limiter = RateLimit({
+  redisURL: "redis://127.0.0.1:6379", // redis connection url
+  expiration: 3600, // 1 hour
+  max: 10, // limit each IP to 10 requests per expiration
+  message: "Too many requests.", // Message that should be sent back to the user, Default is Too many request
+  statusCode: 429, // Status code to be returned. Default is 429
+})
+
+// apply to all requests
+app.use(limiter)
+
+```
+
+# Configuration
+
+- **redisURL**: Redis connection URL
+- **expiration**: second - how long each rate limiting IP address exists for
+- **max**: Max number of connections during expiration before sending a 429 response.
+- **message**: Error message sent to user when max is exceeded. Defaults to 'Too many requests.'
+- **statusCode**: HTTP status code returned when max is exceeded. Defaults to `429`.
+
+# License
+
+MIT © Olohundare Nasirudeen <https://github.com/iamnasirudeen>
---
+++ rateLimiter.js
@@ -0,0 +1,59 @@
+const redis = require("redis");
+
+function apiLimiter(config) {
+  return function (req, res, next) {
+    const ipAdrress =
+      req.headers["x-forwarded-for"] ||
+      req.connection.remoteAddress ||
+      req.socket.remoteAddress ||
+      (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+    const { redisURL, expiration, max, message, statusCode } = config;
+
+    // Make sure max is an Integer
+    if (typeof max !== "number") throw new Error("Max has to be in Integer");
+
+    // Make sure redisURL was specified
+    if (typeof redisURL == undefined)
+      throw new Error("redisURL was not specified");
+
+    // Make sure expiration is an integer
+    if (typeof expiration !== "number")
+      throw new Error("Expiration has to be an Integer");
+
+    // Connect to redis client
+    const client = redis.createClient(redisURL);
+
+    if (req) {
+      client.get(ipAdrress, (err, redisCount) => {
+        if (err) return next(er);
+
+        // convert redisCount from string to an integer
+        let requestAmount = parseInt(redisCount);
+
+        if (redisCount == null) {
+          client.setex(ipAdrress, expiration, 1);
+          return next();
+        }
+
+        if (requestAmount !== undefined && requestAmount < max) {
+          client.incr(ipAdrress);
+          return next();
+        }
+
+        if (requestAmount !== undefined && requestAmount == max) {
+          const response = message == undefined ? "Too many Requests" : message;
+          const status =
+            typeof statusCode !== "number"
+              ? 429
+              : statusCode == undefined
+              ? 429
+              : statusCode;
+          return res.status(status).send({ status, message: response });
+        }
+      });
+    }
+  };
+}
+
+module.exports = apiLimiter;
